75c9b4feb2fd59cc89ec75f0d9f498a055d63f48
chivicoko <victor.c.okoye@gmail.com>
2025-04-04 13:30:21 +0100

    update: api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,22 +5,13 @@ export const sendApiRequest = async <T>(
   url: string,
   data?: T,
   config?: AxiosRequestConfig,
-  // maxRedirects?: number,
 ) => {
-  // console.log("Method:", method);
-  // console.log("URL:", url);
-  // console.log("Data:", data);
-  // console.log("Config:", config);
-  // console.log("MaxRedirects:", maxRedirects);
-
   try {
     const response = await axios({
       method,
       url,
       data,
       ...config,
-      maxBodyLength: Infinity,
-      // maxRedirects,
     });
     return response.data;
   } catch (error) {
